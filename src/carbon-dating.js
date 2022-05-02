e14ddf4430d435afe1d9619cef161e14f1a1f7f6
Zhenya Vasilev <vasilevz195@gmail.com>
2022-05-02 15:07:33 +0600

    feat: done task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(parseInt(sampleActivity)) || parseInt(sampleActivity) <= 0 || parseInt(sampleActivity) >= 15) {
+    return false
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity))) / (Math.log(2) / HALF_LIFE_PERIOD));
 }
-
 module.exports = {
   dateSample
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function countCats(matrix) {
+  let result = 0;
 
+  for (let item of matrix) {
+    for (let i = 0; i < item.length; i++) {
+      if (item[i] === "^^") {
+        result++
+      }
+    }
+  }
+  return result
+}
+countCats([
+  ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+  [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+  [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+  [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+  [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+  [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+  [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+  [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
+]);
 module.exports = {
   countCats
 };
